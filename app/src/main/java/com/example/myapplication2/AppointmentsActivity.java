a49cf120191177e7d788abbdd5e82660ca79eea6
shaili2612 <shailihalani261206@gmail.com>
2026-02-06 21:51:25 -0500

    Add AppointmentsActivity with layout setup

--- app/src/main/java/com/example/myapplication2/AppointmentsActivity.java
+++ app/src/main/java/com/example/myapplication2/AppointmentsActivity.java
@@ -1,1 +1,13 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppointmentsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appointments);
+    }
+}
 
